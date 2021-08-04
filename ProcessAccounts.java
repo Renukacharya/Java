4739c6c6c6a94b5950e745e765dd08a74c628a0f
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2021-08-04 16:49:40 +0530

    Add files via upload
    RandomAccounts

---
+++ ProcessAccounts.java
@@ -0,0 +1,31 @@
+package org.account;
+
+import java.util.Random;
+
+public class ProcessAccounts {
+	
+	public static void main(String args[]) {
+		
+		Random myRandom = new Random();
+		Account anAccount;
+		
+		for(int i=0; i<3; i++) {
+			anAccount = new Account();
+			
+			anAccount.lastName = " " +
+								(char)(myRandom.nextInt(26) + 'A')+
+								(char)(myRandom.nextInt(26) + 'a')+
+								(char)(myRandom.nextInt(26) + 'A')+
+								(char)(myRandom.nextInt(26) + 'a')+
+								(char)(myRandom.nextInt(26) + 'A')+
+								(char)(myRandom.nextInt(26) + 'a')+
+								(char)(myRandom.nextInt(26) + 'A')+
+								(char)(myRandom.nextInt(26) + 'a')+
+								(char)(myRandom.nextInt(26) + 'a');
+			
+			anAccount.id = myRandom.nextInt(10000);
+			anAccount.balance = myRandom.nextInt(1000000);
+			anAccount.display();
+		}
+	}
+}
